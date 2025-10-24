55b1891bd8417bc6851346d504e81e76976158ac
Ashish  wayachal <154004776+Ashishwayachal12@users.noreply.github.com>
2025-10-24 11:50:38 +0530

    Update Conn.java

--- src/com/Ashish/wayachal/Conn.java
+++ src/com/Ashish/wayachal/Conn.java
@@ -1,30 +1,18 @@
-package com.Ashish.wayachal;
-
-
 import java.sql.*;
 
 public class Conn {
-
     Connection c;
     Statement s;
 
-
-    public  Conn()
-    {
-        try
-        {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            c=DriverManager.getConnection("jdbc:mysql:///Travel","root","Ashish@99");
-            s=c.createStatement();
-
-
-        }
-        catch(Exception e)
-        {
+    public Conn() {
+        try {
+            String url = "jdbc:mysql://mysql:3306/travel"; // 'mysql' = service name in docker-compose
+            String user = "root";
+            String password = "Ashish@99";
+            c = DriverManager.getConnection(url, user, password);
+            s = c.createStatement();
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
-
 }
-
-
